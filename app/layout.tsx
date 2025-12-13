06d85d59bf16715e92df75699b3afc3d7a6b0d4e
arkii1 <tommy.mapp@hotmail.com>
2025-12-13 21:03:25 +0000

    Hide overflow caused by sheets

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,18 +12,15 @@ export default function RootLayout({
         <title>Welcome :)</title>
       </head>
       <body
-        className={`antialiased relative`}
+        className={`antialiased relative overflow-hidden`}
       >
         <Navbar />
 
         <main className="flex flex-col items-center justify-center h-screen w-screen p-s">{children}</main>
-        <div className="absolute h-screen w-full overflow-hidden bg-secondary/50 flex items-center justify-center -bottom-1/4 pointer-events-none
-">
-          <div className="">
+        <div className="absolute h-screen w-full overflow-hidden bg-secondary/50 flex items-center justify-center -bottom-1/4 pointer-events-none">
             <div className="sheet sheet-1" />
             <div className="sheet sheet-2" />
             <div className="sheet sheet-3" />
-          </div>
         </div>
 
       </body>
