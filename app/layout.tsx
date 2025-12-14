f39b62f2ae5cca995479e050f0ec99dba5bddf13
arkii1 <tommy.mapp@hotmail.com>
2025-12-14 11:02:22 +0000

    Add improved SEO

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,7 +11,14 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <title>Welcome :)</title>
+        <title>Tommy Mapp | Full-Stack Developer</title>
+        <meta name="description" content="Tommy Mapp — full-stack developer experienced in AWS (SAA-003), .Net, ReactJS, ExpressJS, MySQL and modern development practices. Bristol based. See portfolio to contact me." />
+        <meta property="og:title" content="Tommy Mapp | Portfolio" />
+        <meta property="og:description" content="Tommy Mapp — full-stack developer experienced in AWS (SAA-003), .Net, ReactJS, ExpressJS, MySQL and modern development practices. Bristol based. See portfolio to contact me." />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://tommymapp.dev/" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="canonical" href="https://tommymapp.dev/" />
       </head>
       <body
         className={`antialiased relative overflow-hidden flex flex-col items-center  transition-colors duration-300 ease-in-out`}
